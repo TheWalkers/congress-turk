7aec2e85e8626f582fa9ebca98a5908c16a9ff8e
Timothy Caro-Bruce <tcarobruce@gmail.com>
2016-07-13 12:16:58 -0700

    add office list splitter script

---
+++ split_to_office_hits.py
@@ -0,0 +1,39 @@
+import csv
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=
+        'Generate a CSV for Mech Turk HITs gathering district office data.')
+    parser.add_argument("office_lists",
+        help="CSV results of MTurk district office list HITs.")
+    parser.add_argument("-o", "--out",
+        help="destination for CSV file",
+        type=argparse.FileType('wb', 0), 
+        default='-')  # '-' => stdout
+
+    return parser.parse_args()
+
+
+def convert_row(row):
+    offices = row['Answer.district_offices'].strip().split("\n")
+    for office in offices:
+        yield {'id': row['Input.id'],
+               'url': row['Input.url'],
+               'name': row['Input.name'],
+               'office': office.strip()}
+
+def split_to_office_hits():
+    args = parse_args()
+
+    out_fields = "id url name state office".split()
+    writer = csv.DictWriter(args.out, fieldnames=out_fields)
+    writer.writeheader()
+
+    for row in csv.DictReader(open(args.office_lists)):
+        for row_out in convert_row(row):
+            writer.writerow(row_out)
+
+
+if __name__ == '__main__':
+    split_to_office_hits()
