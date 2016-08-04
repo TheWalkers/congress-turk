e2f2a5012fed4f834767be76ba4228b31038430e
Timothy Caro-Bruce <tcarobruce@gmail.com>
2016-08-04 14:33:57 -0700

    add optional address normalizer

--- reconcile_detail_results.py
+++ reconcile_detail_results.py
@@ -3,6 +3,28 @@ import string
 
 from reconcile_turk_results import TurkResultReconciler
 
+
+def pyaddress_normalize(before):
+    if not before:
+        return before
+    try:
+        addr = parser.parse_address(before)
+    except (UnicodeDecodeError, address.address.InvalidAddressException), e:
+        #print "Error normalizing %s: %s" % (before, e)
+        return before
+    after = " ".join([f for f in
+                      (addr.house_number, addr.street_prefix,
+                       addr.street, addr.street_suffix) if f])
+    return after
+
+try:
+    import address  # https://github.com/SwoopSearch/pyaddress
+    parser = address.AddressParser()
+    normalize_address = pyaddress_normalize
+except ImportError:
+    normalize_address = lambda s: s
+
+
 def remove_empty(s):
     if s == '{}':
         s = ''
@@ -16,20 +38,25 @@ def normalize_phone(phone):
 
 
 class DetailTaskResultReconciler(TurkResultReconciler):
+    STANDARD_PROCESSORS = [remove_empty, remove_not_available]
+    FIELD_PROCESSORS = {
+        'phone': [normalize_phone],
+        'fax': [normalize_phone],
+        'address': [normalize_address],
+    }
 
-    answer_processors = [
-        remove_empty,
-        remove_not_available,
-    ]
+    def equal(self, a, b):
+        return a['Answer.address'] == b['Answer.address']
+
+    def preprocessors(self, field):
+        field = field.replace('Answer.', '')
+        return self.STANDARD_PROCESSORS + self.FIELD_PROCESSORS.get(field, [])
 
     def preprocess_row(self, row):
         for field, answer in self.answers(row).items():
-            for proc in self.answer_processors:
+            for proc in self.preprocessors(field):
                 answer = proc(answer)
             row[field] = answer
-
-        for field in ['Answer.phone', 'Answer.fax']:
-            row[field] = normalize_phone(row[field])
         return row
 
 
