4dc01736ab6e5408068b3d3860e97fffe8e23805
Timothy Caro-Bruce <tim@wawd.com>
2019-03-05 18:41:49 +0000

    Various convert changes: id offices, reorder office fields, use rtyaml, drop offices missing fields

--- convert_office_results.py
+++ convert_office_results.py
@@ -1,7 +1,9 @@
 import argparse
 import csv
-import yaml
-from itertools import groupby
+import re
+import rtyaml as yaml
+from collections import defaultdict, OrderedDict
+from itertools import groupby, count
 
 parser = argparse.ArgumentParser(description=
     'Convert results of a "details" task to yaml')
@@ -26,6 +28,38 @@ rows = list(csv.DictReader(args.source))
 rows.sort(key=by_id)
 grouped = groupby(rows, key=by_id)
 
+FIELD_ORDER = """
+    id
+    address suite building
+    city state zip
+    latitude longitude
+    phone fax hours
+""".split()
+
+
+def reorder_office(office):
+    return OrderedDict([
+        (k, office.get(k))
+        for k in FIELD_ORDER
+        if office.get(k)])
+
+
+office_ids = defaultdict(count)
+
+def id_office(bioguide_id, office):
+
+    locality = office.get('city', 'no_city').lower()
+    locality = re.sub(r'\W', '_', locality)
+
+    office_id = '-'.join([bioguide_id, locality])
+
+    city_count = next(office_ids[office_id])
+    if city_count:
+        office_id = '-'.join([office_id, str(city_count)])
+
+    office['id'] = office_id
+
+
 other_ids = None
 
 # if ids_sources was supplied, build a dict of other ids by bioguide id
@@ -42,7 +76,24 @@ for key, group in grouped:
     for item in group:
         office = {k.replace("Answer.", ""): v
                   for k, v in item.items()
-                  if k.startswith("Answer.")}
+                  if k.startswith("Answer.") and v.strip()}
+
+        if not (office.get('city') and office.get('state') and
+                (office.get('phone') or office.get('address'))):
+            print("Missing minimum fields for office: %r" % office)
+            continue
+
+        id_office(key, office)
+
+        if 'latitude' in item:
+            office['latitude'] = item['latitude']
+        if 'longitude' in item:
+            office['longitude'] = item['longitude']
+
+        if office.get('suite') and re.match(r'^\d+$', office['suite']):
+            office['suite'] = 'Suite ' + office['suite']
+
+        office = reorder_office(office)
         offices.append(office)
 
     offices.sort(key=lambda o: o.get('city'))
@@ -52,5 +103,5 @@ for key, group in grouped:
         ids = other_ids[key]
     data.append({'id': ids, 'offices': offices})
 
-out = yaml.safe_dump(data, default_flow_style=False, encoding='utf-8')
+out = yaml.dump(data)
 args.destination.write(out)
